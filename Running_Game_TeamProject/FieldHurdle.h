054f2d0507b4dff5c7daa93a8cc55310333867f3
qaws1134 <qaws1134@naver.com>
2021-06-15 12:41:29 +0900

    다시 허들 출력

--- Running_Game_TeamProject/FieldHurdle.h
+++ Running_Game_TeamProject/FieldHurdle.h
@@ -3,11 +3,15 @@
 class CFieldHurdle :
 	public CObj
 {
-public:
+private:
 	explicit CFieldHurdle();
+public:
 	virtual ~CFieldHurdle();
 
 public :
+	static CFieldHurdle * Create(_vec3 _vPos, HURDLEID::ID _eID);
+
+	void Set_ID(HURDLEID::ID _eID) { m_eID = _eID; }
 	// CObj을(를) 통해 상속됨
 	virtual HRESULT Ready_Object(void) override;
 	virtual int Update_Object(void) override;
@@ -15,10 +19,16 @@ public :
 	virtual void Render_Object(void) override;
 	virtual void Release_Object(void) override;
 	
+	virtual void Move_Frame()override;
 private:
 	HURDLEID::ID m_eID;
 	BYTE byDrawID;
 	BYTE byOption;
+
+
+
+	// CObj을(를) 통해 상속됨
+	virtual void Moving_Logic(void) override;
 
 };
 
